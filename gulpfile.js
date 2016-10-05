f25112983c5407db84b2c79651e1223e36df1929
Dave Harms <daveharmswebdev@gmail.com>
2016-10-04 23:33:00 -0500

    added source maps

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,16 @@
 const gulp = require('gulp')
 const uglify = require('gulp-uglify')
 const concat = require('gulp-concat')
+const sourcemaps = require('gulp-sourcemaps')
 const ngAnnotate = require('gulp-ng-annotate')
 
 gulp.task('js', function() {
 	gulp.src(['src/module.js', 'src/**/*.js'])
-		.pipe(concat('app.js'))
-		.pipe(ngAnnotate())
-		.pipe(uglify())
+		.pipe(sourcemaps.init())
+			.pipe(concat('app.js'))
+			.pipe(ngAnnotate())
+			.pipe(uglify())
+		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('client/js'))
 })
 
